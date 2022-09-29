53a0a8c79fc90e1b731c81cfcf62de4fffdb4380
openlab <openlab@Z00P2-07.SPSKNM>
2022-09-29 08:37:18 +0200

    start

---
+++ src/main/java/com/example/autoskola_BE/AutoskolaBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.autoskola_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoskolaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoskolaBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/autoskola_BE/AutoskolaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autoskola_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoskolaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
