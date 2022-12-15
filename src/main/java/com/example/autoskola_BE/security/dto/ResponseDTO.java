0ecd798744252b415fb3bc63b7b8bc3e21c7f7b8
martinorion <martinhox03@gmail.com>
2022-12-15 08:29:12 +0100

    always send user Authority

--- src/main/java/com/example/autoskola_BE/security/dto/ResponseDTO.java
+++ src/main/java/com/example/autoskola_BE/security/dto/ResponseDTO.java
@@ -3,9 +3,21 @@ package com.example.autoskola_BE.security.dto;
 public class ResponseDTO {
     private String sessionId;
 
+    public String getCurrentAuthority() {
+        return currentAuthority;
+    }
+
+    public void setCurrentAuthority(String currentAuthority) {
+        this.currentAuthority = currentAuthority;
+    }
+
+    private String currentAuthority;
+
     public String getSessionId() {
         return sessionId;
     }
+
+
 
     public void setSessionId(String sessionId) {
         this.sessionId = sessionId;
--- src/main/java/com/example/autoskola_BE/security/rest/AuthenticationController.java
+++ src/main/java/com/example/autoskola_BE/security/rest/AuthenticationController.java
@@ -3,6 +3,7 @@ package com.example.autoskola_BE.security.rest;
 import com.example.autoskola_BE.security.dto.ResponseDTO;
 import com.example.autoskola_BE.security.dto.UserDTO;
 import com.example.autoskola_BE.security.session.InMemorySessionRegistry;
+import com.example.autoskola_BE.security.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -20,6 +21,9 @@ public class AuthenticationController {
     @Autowired
     public InMemorySessionRegistry sessionRegistry;
 
+    @Autowired
+    public UserRepository userRepository;
+
     @PostMapping("/login")
     public ResponseEntity<ResponseDTO> login(@RequestBody UserDTO user) {
         manager.authenticate(
@@ -27,9 +31,10 @@ public class AuthenticationController {
         );
 
         final String sessionId = sessionRegistry.registerSession(user.getUsername());
+        final String currentAuthority = userRepository.findByUsername(user.getUsername()).getAuthority();
         ResponseDTO response = new ResponseDTO();
         response.setSessionId(sessionId);
-
+        response.setCurrentAuthority(currentAuthority);
         return ResponseEntity.ok(response);
     }
 }
