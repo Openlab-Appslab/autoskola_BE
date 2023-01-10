8bfbfa6c82d50bc8bf1fc4bb21b07a765418c355
martinorion <martinhox03@gmail.com>
2023-01-10 08:45:11 +0100

    add origin 2

--- src/main/java/com/example/autoskola_BE/security/configuration/AppConfig.java
+++ src/main/java/com/example/autoskola_BE/security/configuration/AppConfig.java
@@ -76,7 +76,7 @@ public class AppConfig extends WebSecurityConfigurerAdapter {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+//                        .allowedOrigins("http://localhost:4200")
                         .allowedOrigins("https://autoskola-fe-mda8.vercel.app")
                         .allowedMethods("*")
                         .allowCredentials(true);
